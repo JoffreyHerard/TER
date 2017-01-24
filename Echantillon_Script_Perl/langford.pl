6e8a9b462d24c50b08922d7716cb7249f400f282
JoffreyHerard <joffrey.herard@etudiant.univ-reims.fr>
2017-01-24 12:06:07 +0100

    Update langford.pl

--- Echantillon_Script_Perl/langford.pl
+++ Echantillon_Script_Perl/langford.pl
@@ -1,11 +1,22 @@
 #!/usr/bin/perl
 use v5.14;
+use Cwd 'abs_path';
+use Data::Dumper qw(Dumper);
 
 my $osname = $^O;
 
 if ($osname eq 'linux') {
 	print "We are on Linux...\n";
-	my $nm =`gcc -std=c11 -pedantic -Wall -Wextra -O3    langford.c   -o langford`;
+	my $str = abs_path($0);
+	print $str."\n";
+	my $taille = length $str;
+	my $taille_fichier = length "contraintes.pl";
+	print $taille_fichier."\n";
+	print $taille."\n";
+	
+ 	my $path  = substr $str,0,$taille-$taille_fichier; 
+	print "My path is ; ".$path."\n"; 
+  	system("gcc -std=c11 -pedantic -Wall -Wextra -O3  ".$path."langford.c -o ".$path."langford");
 	exit(3);
 }
 else
